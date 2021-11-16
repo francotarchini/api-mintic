7caab416f40392eeb5e8b5cc6cd4ce5ce8c89f3f
francotarchini <franco.tarchini@gmail.com>
2021-11-16 15:28:21 -0500

    cambie index.js para que utilice el puerto dinamico para desplegar en Heroku

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const cors = require('cors');
 require('./database');
 const app = express();
 
-app.set('port', 5000);
+const PORT = process.env.PORT || 5000;
+app.set('port', PORT);
+//app.set('port', 5000);
 app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json())
